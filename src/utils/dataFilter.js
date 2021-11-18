dd85d75da0435cca6f45f098984cc33321a1c251
kreslovi <i.kreslova@gmail.com>
2021-11-18 13:49:26 +0100

    ADD: catch exception

--- src/utils/dataFilter.js
+++ src/utils/dataFilter.js
@@ -1,7 +1,8 @@
 const every_nth = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1);
 
 const every_eight = (arr) => {
-  console.log(arr);
+  if (!Array.isArray(arr)) return [];
+  
   return every_nth(arr, 8);
 };
 
--- src/utils/weatherController.js
+++ src/utils/weatherController.js
@@ -2,27 +2,47 @@ const baseApiUrl = process.env.REACT_APP_API_BASE_URL;
 const appID = process.env.REACT_APP_MY_API_ID;
 
 function callAPI(url) {
-  return fetch(url).then((response) => response.json());
+  return fetch(url)
+    .then((res) => {
+      if (res.ok) return res.json();
+      else
+        throw Error(
+          `The url:${res.url} returns ${res.statusText}[${res.text}] with status ${res.status}`
+        );
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      return null;
+    });
 }
 
 function getWeather(city, setFunction) {
-  callAPI(`${baseApiUrl}weather?q=${city}&units=metric&appid=${appID}`).then(
-    (data) => setFunction(data)
-  );
+  callAPI(`${baseApiUrl}weather?q=${city}&units=metric&appid=${appID}`)
+    .then((data) => setFunction(data))
+    .catch((error) => {
+      console.error("Error:", error);
+      return null;
+    });
 }
 
 function getForecast(city, setFunction, filter) {
   if (!filter)
-    callAPI(`${baseApiUrl}forecast?q=${city}&units=metric&appid=${appID}`).then(
-      (data) => setFunction(data)
-    );
+    callAPI(`${baseApiUrl}forecast?q=${city}&units=metric&appid=${appID}`)
+      .then((data) => setFunction(data))
+      .catch((error) => {
+        console.error("Error:", error);
+        return null;
+      });
   else
-    callAPI(`${baseApiUrl}forecast?q=${city}&units=metric&appid=${appID}`).then(
-      (data) => {
-        const newArray = filter(data.list);
+    callAPI(`${baseApiUrl}forecast?q=${city}&units=metric&appid=${appID}`)
+      .then((data) => {
+        const newArray = filter(data?.list);
         setFunction(newArray);
-      }
-    );
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        return null;
+      });
 }
 
 export { getWeather, getForecast };
